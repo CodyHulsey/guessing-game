9e29e58908e3f6b69cac03e95dd5ab7cdf5b1d73
CodyHulsey <sendcodyamessage@live.com>
2016-04-09 14:34:21 -0700

    I added the functiont that counts how many questions that they got right.

--- game.js
+++ game.js
@@ -1,40 +1,58 @@
 
 var userName = prompt('What\'s your name?'); //This is asking the user what their name is
 console.log('The visitor\'s name is: ' + userName); //This is logging the visitors answer to the system
+var rightAnswer = 0;
+var wrongAnswer = 0;
+
 var favFood = prompt('Do I like tacos?'); //This is a prompt that pops up and asks if I like tacos.
 if (favFood.toLowerCase() === 'yes') {
   alert('You are correct, ' + userName + '. I love tacos.'); // This will alert the visitor if they typed yes into the entry box.
+  rightAnswer++; //If the user gets the right answer it gets them 1 point.
   console.log('The user entered:' + favFood + ' Which equates to Yes.'); //This is logging the visitors answer to the system
 } else {
     alert('You are incorrect, ' + userName + '. I love Tacos.'); // This is what the viewer will see if they answer anything besides yes
+    wrongAnswer++; //If the user gets the wrong answer it does not give them a point.
     console.log('The user entered: ' + favFood + ' Which equates to No.'); //This is logging the visitors answer to the system
 }
 var favDrink = prompt('Do I like the drink called, "Tom Collins?"'); //This is asking the user if I like Tom Collins'
 if (favDrink.toLowerCase() === 'yes') {
   alert('You are correct, ' + userName + '. That is my drink of choice!'); // This will alert the visitor if they typed yes into the entry box.
+  rightAnswer++; // If the user gets the right answer it gets them 1 point.
   console.log('The user entered:' + favDrink + ' Which equates to Yes.'); //This is logging the visitors answer to the system
 } else {
     alert('You are incorrect, ' + userName + '. Tom Collins is my drink of choice.'); // This is what the viewer will see if they answer anything besides yes
+    wrongAnswer++; //If the user gets the wrong answer it does not give them a point.
     console.log('The user entered: ' + favDrink + ' Which equates to No.'); //This is logging the visitors answer to the system
 }
 var favMusician = prompt('Do I like the musician named Future?');
 if (favMusician.toLowerCase() === 'yes') {
   alert('You are right, ' + userName + '. Future is one of my favorite rappers at this moment.'); // This will alert the visitor if they typed yes into the entry box.
+  rightAnswer++; //If the user gets the right answer it gets them 1 point.
   console.log('The user entered: ' + favMusician + ' Which equates to Yes.'); //This is logging the visitors answer to the system
 } else {
     alert('You\'re wrong, ' + userName + '. I like Future and think he is crushing the rap game right now.'); // This is what the viewer will see if they answer anything besides yes
+    wrongAnswer++; //If the user gets the wrong answer it does not give them a point.
     console.log('The user entered: ' + favMusician + ' Which equates to No.'); //This is logging the visitors answer to the system
 }
 var favNumber = prompt('Guess my favorite number');
 if (favNumber === '13') {
   alert('Woah, ' + userName + '. That is correct. How did you pull that off?');
+  rightAnswer++; //If the user gets the right answer it gets them 1 point.
   console.log('Number input from user Prompt: ' + favNumber); //This is logging the visitors answer to the system
 } else if (favNumber < '13') {
     alert('You guessed: ' + favNumber + '. That is too low.'); // This is what the viewer will see if they answer anything less than 23
+    wrongAnswer++; //If the user gets the wrong answer it does not give them a point.
     console.log('Number input from user Prompt: ' + favNumber); //This is logging the visitors answer to the system
 } else {
     alert('You guessed: ' + favNumber + '. That is too high.'); // This is what the viewer will see if they answer anything more than 23
+    wrongAnswer++; //If the user gets the wrong answer it does not give them a point.
     console.log('Number input from user Prompt: ' + favNumber); //This is logging the visitors answer to the system
+}
+
+if (rightAnswer > wrongAnswer) {
+alert('Congratulations! You got ' + rightAnswer + ' answers correct. You really know me.')
+} else {
+  alert('Bummer! You only got ' + wrongAnswer + ' answers correct. You don\'t know me that well.')
 }
 
 
